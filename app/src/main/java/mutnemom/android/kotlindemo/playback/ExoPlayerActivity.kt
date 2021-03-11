f527a1683acd9e9a150b83c271a88e583cbe89a4
Mutnemom <tt_yordkam@hotmail.com>
2021-03-11 18:11:54 +0700

    Implement exoplayer playback speed

--- app/src/main/java/mutnemom/android/kotlindemo/playback/ExoPlayerActivity.kt
+++ app/src/main/java/mutnemom/android/kotlindemo/playback/ExoPlayerActivity.kt
@@ -5,16 +5,18 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.exoplayer2.DefaultLoadControl
-import com.google.android.exoplayer2.DefaultRenderersFactory
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.TrackGroupArray
 import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
 import mutnemom.android.kotlindemo.databinding.ActivityExoPlayerBinding
+import mutnemom.android.kotlindemo.extensions.toast
 
 class ExoPlayerActivity : AppCompatActivity() {
 
@@ -30,7 +32,7 @@ class ExoPlayerActivity : AppCompatActivity() {
         "https://s3-ap-southeast-1.amazonaws.com/dev.elibrary-private-contents/encoded-media-upload/audio/cf7b8258-309e-48fc-aa4a-eabf317c299c/playlist.m3u8"
 
     private var mediaUrl2 =
-        "https://s3-ap-southeast-1.amazonaws.com/dev.elibrary-private-contents/a32b5f03-2931-40a7-99a6-4a2e0af4a839/encrypted-contents/a2f4557a-61e0-40d9-8096-9fcf46b979d0/05859443-ff9a-4b3d-9158-cbfb30803992.m3u8"
+        "https://s3-ap-southeast-1.amazonaws.com/dev.elibrary-private-contents/a32b5f03-2931-40a7-99a6-4a2e0af4a839/encrypted-contents/364e6720-7526-4417-96de-d66110204b22/b7b80d5b-6666-49ad-93c1-101186173841.m3u8"
 
     private var mediaQuality =
         "https://s3-ap-southeast-1.amazonaws.com/dev.elibrary-private-contents/encoded-media-upload/video/bbf361f4-c10c-4fbe-b0ab-855ebebb271b/playlist.m3u8"
@@ -51,6 +53,7 @@ class ExoPlayerActivity : AppCompatActivity() {
 
         binding = ActivityExoPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setEvent()
     }
 
     override fun onResume() {
@@ -63,8 +66,26 @@ class ExoPlayerActivity : AppCompatActivity() {
         releasePlayback()
     }
 
+    private fun setEvent() {
+        binding.apply {
+            btnSpeed.setOnClickListener { doubleSpeed() }
+
+            btnPause.setOnClickListener { player?.playWhenReady = false }
+            btnPlay.setOnClickListener { player?.playWhenReady = true }
+        }
+    }
+
     private fun initPlayback() {
         if (player == null) {
+            val trackSelection = AdaptiveTrackSelection.Factory(
+                DefaultBandwidthMeter(),
+                1000,
+                2000,
+                2000,
+                2000,
+                .8f
+            )
+
             player = ExoPlayerFactory.newSimpleInstance(
                 DefaultRenderersFactory(this),
                 DefaultTrackSelector(),
@@ -78,6 +99,43 @@ class ExoPlayerActivity : AppCompatActivity() {
 
         val mediaSource = buildMediaSource(Uri.parse(mediaUrl2))
         player?.prepare(mediaSource, true, false)
+        player?.addListener(object : Player.EventListener {
+            override fun onTimelineChanged(timeline: Timeline?, manifest: Any?, reason: Int) {
+            }
+
+            override fun onTracksChanged(
+                trackGroups: TrackGroupArray?,
+                trackSelections: TrackSelectionArray?
+            ) {
+                toast("track changed")
+            }
+
+            override fun onLoadingChanged(isLoading: Boolean) {
+            }
+
+            override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+            }
+
+            override fun onRepeatModeChanged(repeatMode: Int) {
+            }
+
+            override fun onShuffleModeEnabledChanged(shuffleModeEnabled: Boolean) {
+            }
+
+            override fun onPlayerError(error: ExoPlaybackException?) {
+            }
+
+            override fun onPositionDiscontinuity(reason: Int) {
+            }
+
+            override fun onPlaybackParametersChanged(playbackParameters: PlaybackParameters?) {
+                toast("speed: ${playbackParameters?.speed}")
+            }
+
+            override fun onSeekProcessed() {
+            }
+
+        })
     }
 
     private fun releasePlayback() {
@@ -113,6 +171,20 @@ class ExoPlayerActivity : AppCompatActivity() {
             )
             .createMediaSource(uri)
     }
+
+    private fun doubleSpeed() {
+        player?.apply {
+            val currentSpeed = playbackParameters?.speed ?: .5f
+            val expected = currentSpeed * 2
+
+            playbackParameters = PlaybackParameters(expected, 1f)
+        }
+    }
+
+
+
+
+
 
     private fun openLineAt() {
         when (isAppInstalled(this, linePackageName)) {
